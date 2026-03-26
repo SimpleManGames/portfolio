f1acf817ad309d40199894313f862db547b445c9
Riley Smith <riley.smith5345@gmail.com>
2026-03-26 16:43:33 -0700

    bug: scrollbar was present with no content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,12 +48,12 @@ export default function Home() {
   return (
     <>
       <div className="fixed bottom-4 right-4 z-50 flex flex-col gap-2"></div>
-      <div className="min-h-screen relative">
+      <div className="flex-1 relative">
         <div className="fixed inset-0 z-[-1] bg-background">
 
         </div>
 
-        <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+        <div className="mx-auto max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
           <div className="lg:flex lg:justify-between lg:gap-4">
             <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
               <div>
