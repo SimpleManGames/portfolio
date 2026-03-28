de8a7f534c0b2cebef280f8b26642fa0ca944761
Riley Smith <riley.smith5345@gmail.com>
2026-03-27 23:28:54 -0700

    style: clean up unneeded class usages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ import Section from "@/components/section";
 export default function Home() {
   return (
     <>
-      <div className="flex-1 relative">
+      <div className="flex-1">
         <div className="mx-auto max-w-7xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
-          <div className="lg:flex lg:justify-between lg:gap-4">
+          <div className="lg:flex">
             <header className="lg:fixed lg:py-24">
               <div>
                 <h1 className="text-4xl font-bold tracking-tight text-foreground sm:text-5xl">
@@ -33,7 +33,7 @@ export default function Home() {
                 <Nav />
               </div>
             </header>
-            <main className="pt-24 lg:w-1/2 lg:ml-auto lg:py-24 space-y-24">
+            <main className="pt-24 lg:w-1/2 lg:ml-auto lg:py-24">
               <Section id="about">
                 <div className="text-muted-background font-mono leading-relaxed">
                   <p className="mb-6">
