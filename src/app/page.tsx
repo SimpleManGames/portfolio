80fcabf025b5c1e4388f4a52d861300f9606de9e
Riley Smith <riley.smith5345@gmail.com>
2026-03-27 20:56:28 -0700

    fix: left column no longer scrolls slightly when scrolling main content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="flex-1 relative">
         <div className="mx-auto max-w-7xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
           <div className="lg:flex lg:justify-between lg:gap-4">
-            <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+            <header className="lg:fixed lg:py-24">
               <div>
                 <h1 className="text-4xl font-bold tracking-tight text-foreground sm:text-5xl">
                   Riley Smith
@@ -33,7 +33,7 @@ export default function Home() {
                 <Nav />
               </div>
             </header>
-            <main className="pt-24 lg:w-1/2 lg:py-24 space-y-24">
+            <main className="pt-24 lg:w-1/2 lg:ml-auto lg:py-24 space-y-24">
               <Section id="about">
                 <div className="text-muted-background font-mono leading-relaxed">
                   <p className="mb-6">
