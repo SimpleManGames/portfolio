7687cc802b39d1061a008fbc12b027ebadbaff52
Riley Smith <riley.smith5345@gmail.com>
2026-03-28 15:18:30 -0700

    feat: add logo to project overview section

--- src/app/experience/proprio/page.tsx
+++ src/app/experience/proprio/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Section from "@/components/section";
 
 export default function Page() {
@@ -7,24 +8,35 @@ export default function Page() {
         <h2 className="text-2xl font-bold text-foreground mb-4">
           Project Overview
         </h2>
-        <div className="text-muted-foreground font-mono leading-relaxed space-y-4">
-          <p>
-            Proprio Vision&apos;s mission was to deliver better outcomes for
-            patients — less radiation, smaller insurance bills, and quicker
-            surgeries.
-          </p>
-          <p>
-            We achieved this by developing the Paradigm System. Using an array
-            of cameras, sensors, and tracked instruments, we replicated the
-            real-world surgical environment in 3D space, giving surgeons a
-            clearer look into the patient than ever before.
-          </p>
-          <p>
-            Building on this capability, we developed Intra-Op Measurements — a
-            first of its kind. Surgeons could get real-time alignment data
-            without the radiation exposure and significant time cost of
-            traditional methods.
-          </p>
+        <div className="flex flex-col sm:flex-row gap-6">
+          <div className="text-muted-foreground font-mono leading-relaxed space-y-4 flex-1">
+            <div>
+              <Image
+                src="/experience/proprio/proprio_vision_icon.svg"
+                alt="Proprio"
+                className="float-left mt-4 ml-6 mr-6 mb-4 rounded-xl object-contain bg_w-48"
+                width={192}
+                height={192}
+              ></Image>
+              <p>
+                Proprio Vision's mission was to deliver better outcomes for
+                patients — less radiation, smaller insurance bills, and quicker
+                surgeries.
+              </p>
+            </div>
+            <p>
+              We achieved this by developing the Paradigm System. Using an array
+              of cameras, sensors, and tracked instruments, we replicated the
+              real-world surgical environment in 3D space, giving surgeons a
+              clearer look into the patient than ever before.
+            </p>
+            <p>
+              Building on this capability, we developed Intra-Op Measurements —
+              a first of its kind. Surgeons could get real-time alignment data
+              without the radiation exposure and significant time cost of
+              traditional methods.
+            </p>
+          </div>
         </div>
       </Section>
 
